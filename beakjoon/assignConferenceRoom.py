b98b8e72e5fb7166ab40816cebdb690b66a8cca9
all-day-and-night <jwug0821@gmail.com>
2021-12-08 18:48:12 +0900

    beakjoon 1931 greedy

--- beakjoon/assignConferenceRoom.py
+++ beakjoon/assignConferenceRoom.py
@@ -5,7 +5,9 @@ for i in range(N):
   s, e = map(int, sys.stdin.readline().split())
   time[i][0] = s
   time[i][1] = e
-time.sort(key = lambda x: (x[1], x[0])) cnt = 1 end_time = time[0][1]
+time.sort(key = lambda x: (x[1], x[0]))
+cnt = 1
+end_time = time[0][1]
 
 for i in range(1, N):
   if time[i][0] >= end_time:
